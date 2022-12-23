68aa3ff4904526937bb91336b7c381944fe22c06
Nguye <75427876+nguyenphu-3204@users.noreply.github.com>
2022-12-23 16:02:05 +0700

    Update source_code.cpp

--- source_code.cpp
+++ source_code.cpp
@@ -2,17 +2,22 @@
 #include "matrix.h"
 using namespace std;
 
-
 int main(){
     matrix A, B;
     A.set_matrix();
-    B.set_matrix();
+    A.set_matrix();
     A.print_matrix();
     B.print_matrix();
+    cout << "C = A*3\n";
     (A*3).print_matrix();
+    cout << "C = A+B\n";
     (A+B).print_matrix();
+    cout << "C = A-B\n";
+    (A-B).print_matrix();
+    cout << "C = A*B\n";
     (A*B).print_matrix();
-    (B+A).print_matrix();
+    cout << "C = B*A\n";
+    (B*A).print_matrix();
     system("pause");
 return 0;
 }
